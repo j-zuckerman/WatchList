cd0cd29cb37e4b1406156e510ea69eb995b8676f
James Zuckerman <jzuckerman4@gmail.com>
2020-10-11 13:20:00 -0700

    Implementing side menu

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { MovieContext } from '../../context/movies';
 import styles from './SideMenu.module.css';
 
@@ -7,27 +7,49 @@ export const SideMenu = () => {
     MovieContext
   );
 
+  //used to keep track of what link is active
+  const [activeKey, setActiveKey] = useState('top_rated');
+
   const switchToTopRated = () => {
     fetchTopRated();
+    setActiveKey('top_rated');
   };
 
   const switchToMostPopular = () => {
     fetchMostPopular();
+    setActiveKey('most_popular');
   };
 
   const switchToNowPlaying = () => {
     fetchNowPlaying();
+    setActiveKey('now_playing');
   };
+
   return (
     <nav>
       <ul className={styles.sideMenu}>
-        <li className={styles.sideMenu__item} onClick={switchToTopRated}>
+        <li
+          className={`${styles.sideMenu__item} ${
+            activeKey === 'top_rated' ? styles.active : ''
+          }`}
+          onClick={switchToTopRated}
+        >
           Top Rated
         </li>
-        <li className={styles.sideMenu__item} onClick={switchToMostPopular}>
+        <li
+          className={`${styles.sideMenu__item} ${
+            activeKey === 'most_popular' ? styles.active : ''
+          }`}
+          onClick={switchToMostPopular}
+        >
           Most Popular
         </li>
-        <li className={styles.sideMenu__item} onClick={switchToNowPlaying}>
+        <li
+          className={`${styles.sideMenu__item} ${
+            activeKey === 'now_playing' ? styles.active : ''
+          }`}
+          onClick={switchToNowPlaying}
+        >
           Now Playing
         </li>
         <li className={styles.sideMenu__item}></li>
