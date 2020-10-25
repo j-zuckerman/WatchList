420a83f54577a1a6f24815b81686fca825d964b3
James Zuckerman <jzuckerman4@gmail.com>
2020-10-25 00:42:49 -0700

    Updates

---
+++ precache-manifest.c58914c32920bd85ee597e481cc56b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf4a4baf0d348baa266fe8d786d8df1",
+    "url": "/WatchList/index.html"
+  },
+  {
+    "revision": "9a9517a4311e9fdd95a6",
+    "url": "/WatchList/static/css/main.cf89add5.chunk.css"
+  },
+  {
+    "revision": "70e41d4b4e5b6b3c49b7",
+    "url": "/WatchList/static/js/2.71b9755b.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/WatchList/static/js/2.71b9755b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9517a4311e9fdd95a6",
+    "url": "/WatchList/static/js/main.2b0eb37c.chunk.js"
+  },
+  {
+    "revision": "957e209af2d7deab9b3e",
+    "url": "/WatchList/static/js/runtime-main.0fc3e483.js"
+  }
+]);
